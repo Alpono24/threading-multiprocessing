0c9480530d24a63987595c1ab23a7e5388e57432
Ваше Имя <you@example.com>
2025-12-06 16:06:10 +0300

    Home_task_2_information_about_calculations and code

---
+++ Home_task_2_part_1.py
@@ -0,0 +1,76 @@
+
+
+# Задание 2. Аналогичная задача: три программы, одна
+# последовательная, другая поточная, третья процессная. Но они
+# занимаются не математикой, а GET-запросами на любые юрлки.
+# Итог. По заданию 1 замерить время выполнения всех трех
+# программ, сравнить. По заданию 2 тоже замерить и сравнить.
+# Сделать выводы
+
+import time
+import requests
+import logging
+
+
+logging.basicConfig(
+    filename='Home_task_2_information_about_calculations.log',
+    filemode='a',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+
+def download_content(url):
+    try:
+        response= requests.get(url)
+        if response.ok:
+            logging.info(f"Содержимое {url} имеет размер {len(response.text)} символов")
+            return response
+        else:
+            logging.error(f'Ошибка при загрузке {url}: {response.status_code}')
+            return None
+    except  requests.RequestException as e:
+        logging.error(f'Ошибка при загрузке {url}: {e}')
+        return None
+
+urls = [
+    'https://www.onliner.by/',
+    'https://realt.onliner.by/2025/12/06/minsk-iz-starogo-alboma',
+    'https://tech.onliner.by/2025/12/06/astrapoglyad-golosovanie',
+    'https://www.nbrb.by/apihelp/exrates',
+    'https://auto.onliner.by/2025/12/06/autoshow-la',
+    'https://catalog.onliner.by/',
+    'https://r.onliner.by/pk/',
+    'https://www.onliner.by/',
+    'https://realt.onliner.by/2025/12/06/minsk-iz-starogo-alboma',
+    'https://tech.onliner.by/2025/12/06/astrapoglyad-golosovanie',
+    'https://www.nbrb.by/apihelp/exrates',
+    'https://auto.onliner.by/2025/12/06/autoshow-la',
+    'https://catalog.onliner.by/',
+    'https://r.onliner.by/pk/',
+]
+
+
+function_start_time = time.time()
+
+logging.info(f'Вариант 1 - последовательное выполнение/последовательный код: Работа функции download_content.')
+
+for url in urls:
+    response = download_content(url)
+    # if response:
+    #     logging.info(f'Содержимое {url} успешно загружено!')
+    # else:
+    #     logging.info(f'Ошибка в загрузке {url}')
+
+function_end_time = time.time()
+function_operation_time = function_end_time - function_start_time
+
+
+hours = int(function_operation_time // 3600)
+minutes = int((function_operation_time % 3600) // 60)
+seconds = int(function_operation_time % 60)
+formatted_time = f'{hours:02d}:{minutes:02d}:{seconds:02d}'
+
+logging.info(f'Время выполнения: {formatted_time}.')
+print(f'Время выполнения: {formatted_time}.')
+
--- Home_task_2_part_3.py
+++ Home_task_2_part_3.py
@@ -1,1 +1,92 @@
 
+# Задание 2. Аналогичная задача: три программы, одна
+# последовательная, другая поточная, третья процессная. Но они
+# занимаются не математикой, а GET-запросами на любые юрлки.
+# Итог. По заданию 1 замерить время выполнения всех трех
+# программ, сравнить. По заданию 2 тоже замерить и сравнить.
+# Сделать выводы
+import os
+import time
+import requests
+import logging
+import multiprocessing
+
+logging.basicConfig(
+    filename='Home_task_2_information_about_calculations.log',
+    filemode='a',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+
+def download_content(url):
+    try:
+        response= requests.get(url)
+        if response.ok:
+            logging.info(f"Содержимое {url} имеет размер {len(response.text)} символов")
+            return response
+        else:
+            logging.error(f'Ошибка при загрузке {url}: {response.status_code}')
+            return None
+    except  requests.RequestException as e:
+        logging.error(f'Ошибка при загрузке {url}: {e}')
+        return None
+
+def worker(url):
+    result = download_content(url)
+    return result
+
+if __name__ == '__main__':
+    logging.info(f'Вариант 3 - multiprocessing: Работа функции download_content.')
+
+    with multiprocessing.Pool(processes=os.cpu_count()) as pool:
+        function_start_time = time.time()
+
+        urls = [
+            'https://www.onliner.by/',
+            'https://realt.onliner.by/2025/12/06/minsk-iz-starogo-alboma',
+            'https://tech.onliner.by/2025/12/06/astrapoglyad-golosovanie',
+            'https://www.nbrb.by/apihelp/exrates',
+            'https://auto.onliner.by/2025/12/06/autoshow-la',
+            'https://catalog.onliner.by/',
+            'https://r.onliner.by/pk/',
+            'https://www.onliner.by/',
+            'https://realt.onliner.by/2025/12/06/minsk-iz-starogo-alboma',
+            'https://tech.onliner.by/2025/12/06/astrapoglyad-golosovanie',
+            'https://www.nbrb.by/apihelp/exrates',
+            'https://auto.onliner.by/2025/12/06/autoshow-la',
+            'https://catalog.onliner.by/',
+            'https://r.onliner.by/pk/',
+        ]
+
+        results = pool.map(worker, urls)
+        # for url in urls:
+        #     if results:
+        #         logging.info(f'Содержимое {url} успешно загружено!')
+        #     else:
+        #         logging.info(f'Ошибка в загрузке {url}')
+
+        function_end_time = time.time()  #
+        function_operation_time = function_end_time - function_start_time
+
+        hours = int(function_operation_time // 3600)
+        minutes = int((function_operation_time % 3600) // 60)
+        seconds = int(function_operation_time % 60)
+        formatted_time = f'{hours:02d}:{minutes:02d}:{seconds:02d}'
+
+
+        logging.info(f'Время выполнения: {formatted_time}.')
+        print(f'Время выполнения: {formatted_time}.')
+
+
+
+
+
+
+
+
+
+
+
+
+
